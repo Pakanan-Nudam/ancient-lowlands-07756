3cee1d33daf5c49cbd3e729b475677554a3754f2
Pakanan-Nudam <71056615+Pakanan-Nudam@users.noreply.github.com>
2021-01-04 15:48:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,46 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        .block-1{
+            width: 200px;
+            margin: 20px;
+            background: pink;
+            float: left;
+        }
+
+        .block-2{
+            width: 300px;
+            margin: 20px;
+            background: green;
+            float: left;
+        }
+
+        .block-3{
+            width: 200px;
+            margin: 20px;
+            background: gold;
+            float: left;
+        }
+    </style>
+
+</head>
+<body>
+    
+    <div class="block-1">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus veniam magnam adipisci? Error magni culpa quo consequuntur deserunt, modi magnam maxime, eaque animi, nihil vero quibusdam? Rerum nam inventore non.
+    </div>
+    <div class="block-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus veniam magnam adipisci? Error magni culpa quo consequuntur deserunt, modi magnam maxime, eaque animi, nihil vero quibusdam? Rerum nam inventore non.
+    </div>
+    <div class="block-3">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus veniam magnam adipisci? Error magni culpa quo consequuntur deserunt, modi magnam maxime, eaque animi, nihil vero quibusdam? Rerum nam inventore non.
+    </div>
+
+</body>
+</html>
